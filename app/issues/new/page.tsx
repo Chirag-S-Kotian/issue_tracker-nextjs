78eec841fcaefd6b6682deb4f7eca4ca582a2448
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-10 10:40:17 +0530

    Build the new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import React from "react";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+MagnifyingGlassIcon;
+
+const NewIssuePage = () => {
+  return (
+    <div className="max-w-xl space-y-3">
+      <TextField.Root placeholder="Title">
+        <TextField.Slot>
+          <MagnifyingGlassIcon height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
+};
+
+export default NewIssuePage;
