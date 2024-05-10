5ff244c0283af5f556ae8883c2bd0e44c1f6ad19
Chirag-S-kotian <ckotian117@gmail.com>
2024-05-10 11:54:32 +0530

    "Update TextField to Radix UI Text and add error messages for title and description fields"
    This commit updates the TextField component from the previous Radix UI theme to the new Radix UI Text component. Additionally, error messages for the

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -16,7 +16,12 @@ type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit } = useForm<IssueForm>({
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
@@ -43,6 +48,11 @@ const NewIssuePage = () => {
             <MagnifyingGlassIcon />
           </TextField.Slot>
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -50,7 +60,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
         <Button>Submit New Issue</Button>
       </form>
     </div>
